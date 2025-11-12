b884500fb1bb20d9a4fe9b8761017bba121ce55c
Koen <98043234+koen1711@users.noreply.github.com>
2025-11-12 21:08:52 +0100

    fix: add ownCurriculum filter (#2)

--- src/lib/Curriculum.ts
+++ src/lib/Curriculum.ts
@@ -4,6 +4,7 @@ export type Activity = {
     level: "Beginner" | "Medium" | "Advanced";
     link: string;
     order: number;
+		ownMaterials: boolean;
 }
 
 export let curriculum: Activity[] = [
@@ -12,23 +13,28 @@ export let curriculum: Activity[] = [
         labels: ["Python", "Game Development", "2D"],
         level: "Beginner",
         link: "https://play.coderius.nl/",
-        order: 1
+        order: 1,
+				ownMaterials: true
     },
     {
         title: "Robotica",
         labels: ["Robotics", "Python", "Building"],
         level: "Beginner",
         link: "https://robotica.coderius.nl/",
-        order: 2
+        order: 2,
+				ownMaterials: true
     },
     {
         title: "Damn Vulnerable Web Application",
         labels: ["Security", "Web"],
         level: "Medium",
         link: "https://dvwa.coderius.nl/",
-        order: 5
+        order: 5,
+				ownMaterials: false
     }
 ];
+
+export let ownCurriculum: Activity[] = curriculum.filter(activity => activity.ownMaterials);
 
 export let levelColors: { [key in Activity["level"]]: string } = {
     "Beginner": "green",
